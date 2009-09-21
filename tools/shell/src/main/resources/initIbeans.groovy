16d2e1e8aa366ba7e724e5afee03859c38843f67
ross <ross@9ce7195a-5073-0410-997c-d46855c5cba6>
2009-09-21 22:25:02 +0000

    Use IBeans hooks

--- tools/shell/src/main/resources/initIbeans.groovy
+++ tools/shell/src/main/resources/initIbeans.groovy
@@ -1,14 +1,14 @@
 import org.codehaus.groovy.tools.shell.Shell
 import org.mule.config.builders.DefaultsConfigurationBuilder
-import org.mule.context.DefaultMuleContextBuilder
-import org.mule.context.DefaultMuleContextFactory
 import org.mule.ibeans.IBeansContext
 import org.mule.ibeans.config.IBeanHolderConfigurationBuilder
+import org.mule.ibeans.internal.config.IBeansMuleContextFactory
+import org.mule.ibeans.internal.config.IBeansMuleContextBuilder
 
 //Usful to have avaialable in the shell
 Shell shell = (Shell) binding.getVariable("shell")
 assert shell != null
 
 //No support for Guice right now
-muleContext = new DefaultMuleContextFactory().createMuleContext([new DefaultsConfigurationBuilder(), new IBeanHolderConfigurationBuilder(shell.interp.classLoader)], new DefaultMuleContextBuilder())
-ibeans = new IBeansContext(muleContext)
+muleContext = new IBeansMuleContextFactory().createMuleContext([new DefaultsConfigurationBuilder(), new IBeanHolderConfigurationBuilder(shell.interp.classLoader)], new IBeansMuleContextBuilder())
+ibeans = muleContext.getRegistry().lookupObject(IBeansContext.class)
