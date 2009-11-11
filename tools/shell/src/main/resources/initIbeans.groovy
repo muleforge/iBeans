d4ebe965df244d5b0339ab9e28d1a73530606fea
ross <ross@9ce7195a-5073-0410-997c-d46855c5cba6>
2009-11-11 20:37:50 +0000

    added common imports

--- tools/shell/src/main/resources/initIbeans.groovy
+++ tools/shell/src/main/resources/initIbeans.groovy
@@ -4,6 +4,9 @@ import org.mule.ibeans.IBeansContext
 import org.mule.ibeans.config.IBeanHolderConfigurationBuilder
 import org.mule.ibeans.internal.config.IBeansMuleContextFactory
 import org.mule.ibeans.internal.config.IBeansMuleContextBuilder
+import static org.mule.ibeans.IBeansSupport.select;
+import static org.mule.ibeans.IBeansSupport.selectValue;
+import org.w3c.dom.Document;
 
 //Usful to have avaialable in the shell
 Shell shell = (Shell) binding.getVariable("shell")
