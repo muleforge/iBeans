7849c7aa126ad9d3f896e241df99c8a393feb3aa
ross <ross@9ce7195a-5073-0410-997c-d46855c5cba6>
2009-11-24 23:47:43 +0000

    Added Generics support to the JaxBContextResolver

--- modules/xml/src/main/java/org/mule/ibeans/module/xml/JAXBContextResolver.java
+++ modules/xml/src/main/java/org/mule/ibeans/module/xml/JAXBContextResolver.java
@@ -11,9 +11,12 @@ package org.mule.ibeans.module.xml;
 
 import org.mule.api.MuleContext;
 import org.mule.ibeans.internal.ObjectResolver;
+import org.mule.ibeans.internal.util.generics.GenericsUtil;
+import org.mule.ibeans.internal.util.generics.MethodParameter;
 import org.mule.utils.AnnotationUtils;
 
 import java.lang.reflect.Method;
+import java.util.Collection;
 
 import javax.xml.bind.JAXBContext;
 
@@ -38,14 +41,15 @@ public class JAXBContextResolver implements ObjectResolver
     public Object findObject(Class type, Method method, MuleContext context) throws Exception
     {
         JAXBContext jax;
-        Class annotatedType = method.getReturnType();
+        Class annotatedType = checkCollectionReturnType(method);
+
         boolean isJAXB = AnnotationUtils.hasAnnotationWithPackage("javax.xml.bind.annotation", annotatedType);
         int i = 0;
         if (!isJAXB)
         {
             for (int j = 0; j < method.getParameterTypes().length; j++)
             {
-                annotatedType = method.getParameterTypes()[j];
+                annotatedType = checkCollectionType(new MethodParameter(method, j));
                 isJAXB = AnnotationUtils.hasAnnotationWithPackage("javax.xml.bind.annotation", method.getParameterTypes()[j]);
                 if (isJAXB)
                 {
@@ -63,9 +67,35 @@ public class JAXBContextResolver implements ObjectResolver
         if (jax == null)
         {
             logger.info("No common JAXB context configured, creating a local one for: " + method);
-            jax = JAXBContext.newInstance(annotatedType);
+            jax = JAXBContext.newInstance(annotatedType.getPackage().getName());
         }
         return jax;
 
+    }
+
+    protected Class checkCollectionReturnType(Method m)
+    {
+        if(Collection.class.isAssignableFrom(m.getReturnType()))
+        {
+            Class tempType = GenericsUtil.getCollectionReturnType(m);
+            if(tempType!=null)
+            {
+                return tempType;
+            }
+        }
+        return m.getReturnType();
+    }
+
+    protected Class checkCollectionType(MethodParameter param)
+    {
+        if(Collection.class.isAssignableFrom(param.getParameterType()))
+        {
+            Class tempType = GenericsUtil.getCollectionParameterType(param);
+            if(tempType!=null)
+            {
+                return tempType;
+            }
+        }
+        return param.getParameterType();
     }
 }
