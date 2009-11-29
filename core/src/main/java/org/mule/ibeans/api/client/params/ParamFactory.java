5693187ba7362b00fd075151b497341efbc93929
ross <ross@9ce7195a-5073-0410-997c-d46855c5cba6>
2009-11-29 23:43:41 +0000

    added Javadoc

--- core/src/main/java/org/mule/ibeans/api/client/params/ParamFactory.java
+++ core/src/main/java/org/mule/ibeans/api/client/params/ParamFactory.java
@@ -10,12 +10,15 @@ public interface ParamFactory
 
     /**
      * The method used to create the parameter.
-     * Note that if null is returned, the parameter is omitted from the request.
+     * Note that if null is returned, the parameter is omitted from the request. include the parameter in the request with a
+     * null value return and empty String. If the optional flag is false, returning a null will cause an exception.
      *
-     * @param paramName
-     * @param optional
-     * @param invocationContext
-     * @return
+     * @param paramName the parameter name associated with the value
+     * @param optional determines whether this parameter is optional. If optional a null can be returned from this method
+     * @param invocationContext the current invocation context.  Can be used to look up additional information about the
+     * request such as the other invocation parameters and properties
+     * @return an object associated to the param name that will be used to construct the request or null if the parameter is
+     * optional and could not be created. 
      */
     Object create(String paramName, boolean optional, InvocationContext invocationContext);
 }
