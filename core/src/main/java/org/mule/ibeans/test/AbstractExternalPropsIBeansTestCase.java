4341a450c8e79bbe84e995de3e2383e3675ea277
ross <ross@9ce7195a-5073-0410-997c-d46855c5cba6>
2010-02-11 18:15:55 +0000

    Add the external props to the registry so that they can be used a sproperty placeholders in the code too

--- core/src/main/java/org/mule/ibeans/test/AbstractExternalPropsIBeansTestCase.java
+++ core/src/main/java/org/mule/ibeans/test/AbstractExternalPropsIBeansTestCase.java
@@ -11,14 +11,13 @@ package org.mule.ibeans.test;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 /**
  * A Unit test that extends the {@link org.mule.ibeans.test.AbstractIBeansTestCase} to load properties from a
  * location specified by a system property. These properties reside on the local machine (usually contain sensitive
  * information.
- * <p/>
- * Implementors of this class should call {@link #checkProperties()} at the beginning of every test call
  * <p/>
  * In order to use this class for testing in Maven, you will need to  configure the sure-fire plugin to regconise
  * the {@link #IBEANS_TEST_PROPERTIES} property since sure-fire will fork tests in their own JVM.  To do this, add
@@ -43,12 +42,10 @@ import java.util.Properties;
 public class AbstractExternalPropsIBeansTestCase extends AbstractIBeansTestCase
 {
     public static final String IBEANS_TEST_PROPERTIES = "ibeans.test.properties";
-
-    private Properties properties;
-
+    public static final String DEFAULT_PROPERTIES_FILENAME = "build.properties";
 
     @Override
-    protected void doSetUp() throws Exception
+    protected void addStartUpProperties(Properties properties)
     {
         String path = System.getProperty(IBEANS_TEST_PROPERTIES);
         if (path == null)
@@ -58,7 +55,7 @@ public class AbstractExternalPropsIBeansTestCase extends AbstractIBeansTestCase
         }
         if (path == null)
         {
-            path = "build.properties";
+            path = DEFAULT_PROPERTIES_FILENAME;
         }
 
         File f = new File(path);
@@ -66,27 +63,19 @@ public class AbstractExternalPropsIBeansTestCase extends AbstractIBeansTestCase
         {
             fail("Test properties not found at: " + f.getAbsolutePath() + ". " + IBEANS_TEST_PROPERTIES + " set to: " + path);
         }
-        properties = new Properties();
-        properties.load(new FileInputStream(f));
-
-    }
-
-    protected void checkProperties()
-    {
-        if (properties == null)
+        try
         {
-            fail("Local build.properties containing test config is not present, contact Ross");
+            properties.load(new FileInputStream(f));
         }
+        catch (IOException e)
+        {
+            fail("could not load properties for test: " + f.getAbsolutePath() + ", " + e.getMessage());
+        }
+
     }
 
-    protected Properties getProps()
+    protected Object get(String key)
     {
-        return properties;
-    }
-
-    protected String get(String key)
-    {
-        checkProperties();
-        return properties.getProperty(key);
+        return iBeansContext.getConfig().get(key);
     }
 }
